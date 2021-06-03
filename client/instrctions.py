9b72b746168137a3c35587e310090bb8f8e2ab9b
graham <grey.ham.acre@gmail.com>
2021-06-03 09:06:21 +0200

    add practice stims

--- client/instrctions.py
+++ client/instrctions.py
@@ -2,27 +2,24 @@ INTRO_HEADER = "Vous allez jouer à un jeu d'association."
 
 INTRO_DETAILS = "Les règles:\n\n" \
                 "Le joueur 1 voit une image et doit la nommer. Le joueur 2 doit répondre par un mot qui lui vient en premier à l'esprit mais qui est dans la même catégorie que celui produit par le joueur 1 auparavant.\n" \
-                "Il y aura deux catégories - les animaux et les outils. Ainsi, par exemple, un joueur voit une image d'un lion et dit “lion”, l'autre joueur doit répondre en prononçant n'importe quel autre mot animal, par exemple, “chien”.\n" \
-                "Avant chaque procès, vous entendrez soit un simple bip, soit une partie d'une phrase, mais la tâche reste la même, peu importe ce que vous entendez auparavant.\n" \
+                "Chaque essai implique donc les 2 joueurs, chacun prononçant un mot.\n" \
+                "Il y aura deux catégories - les animaux et les outils. Donc, si le joueur 1 nomme un animal, vous devez répondre avec un autre mot animal, et si le joueur 1 nomme un outil, vous devez répondre avec un autre mot outil.\n" \
+                "Avant chaque essai, vous entendrez soit un simple bip, soit une partie d'une phrase, mais la tâche reste la même, peu importe ce que vous entendez auparavant.\n" \
                 "Les rôles du Joueur 1 et du Joueur 2 seront échangés à chaque essai, donc si à l'essai 1 vous avez vu une image et l'avez nommée, à l'essai 2 ce sera votre partenaire qui verra une image et la nommera.\n" \
-                "Il y aura 10 essais pratiques avant le début de l'expérience proprement dite.\n" \
+                "Il y aura 6 essais pratiques avant le début de l'expérience proprement dite.\n" \
                 "Il y aura également des pauses où vous pourrez vous reposer.\n\n" \
                 "S'il vous plaît, appuyez sur la barre d'espace lorsque vous êtes prêt à commencer.\n"
 
 # ---------
 
 
-PRACTICE_COMPLETE_HEADER = "Practice complete"
+PRACTICE_COMPLETE_HEADER = "fin de entraînement"
 
-PRACTICE_COMPLETE_DETAILS = "Please have a break\n\n" \
-                            "Le joueur 1 voit une image et doit la nommer. Le joueur 2 doit répondre par un mot qui lui vient en premier à l'esprit mais qui est dans la même catégorie que celui produit par le joueur 1 auparavant.\n\n" \
-                            "S'il vous plaît, appuyez sur la barre d'espace lorsque vous êtes prêt à commencer.\n"
+PRACTICE_COMPLETE_DETAILS = "C'est la fin des essais entraînement. Appuyez sur la barre d'espace lorsque vous êtes prêt à commencer l'expérience.\n"
 
 # ---------
 
 
-BREAK_HEADER = "Break time"
+BREAK_HEADER = "Pause"
 
-BREAK_DETAILS = "Please have a break\n\n" \
-                "Le joueur 1 voit une image et doit la nommer. Le joueur 2 doit répondre par un mot qui lui vient en premier à l'esprit mais qui est dans la même catégorie que celui produit par le joueur 1 auparavant.\n\n" \
-                "S'il vous plaît, appuyez sur la barre d'espace lorsque vous êtes prêt à commencer.\n"
+BREAK_DETAILS = "Vous pouvez faire une pause maintenant. Appuyez sur la barre d'espace lorsque vous êtes prêt à continuer.\n"
